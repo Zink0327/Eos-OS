eb35afb0c3ab2049fbc2265cc994b565e175e9a6
Zink0327 <122519365+Zink0327@users.noreply.github.com>
2023-12-05 12:29:46 +0800

    Create Makefile.bat

---
+++ bootloader/floppy/Makefile.bat
@@ -0,0 +1,50 @@
+::Eos Makefile
+
+::   Copyright (C) 2023 by Zink
+::   This file is part of Eos
+::
+::    Eos is free software: you can redistribute it and/or modify
+::    it under the terms of the GNU General Public License as published by
+::    the Free Software Foundation, either version 3 of the License, or
+::    (at your option) any later version
+::
+::    Eos is distributed in the hope that it will be useful,
+::    but WITHOUT ANY WARRANTY; without even the implied warranty of
+::    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::    GNU General Public License for more details.
+
+::    You should have received a copy of the GNU General Public License
+::    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+::src/bootloader/floppy/Makefile.bat
+
+@echo off
+echo entering directory %~dp0
+if "%1" equ "clean" (goto clean) else (goto build)
+
+:clean
+del *.bin
+del *.lst
+goto end
+
+:build
+if exist %~dp0..\..\tools\nasm\nasm.exe (
+%~dp0..\..\tools\nasm\nasm.exe %~dp0\mbr.asm -o %~dp0..\..\build\mbr.bin -l %~dp0..\..\build\mbr.lst
+echo %~dp0..\..\tools\nasm\nasm.exe %~dp0\mbr.asm -o %~dp0..\..\build\mbr.bin -l %~dp0..\..\build\mbr.lst
+if not exist %~dp0..\..\build\mbr.bin (
+echo compile error!
+goto end
+)
+echo %~dp0..\..\tools\nasm\nasm.exe %~dp0\loader.asm -i %~dp0 -o %~dp0..\..\build\loader.bin -l %~dp0..\..\build\loader.lst
+%~dp0..\..\tools\nasm\nasm.exe %~dp0\loader.asm -i %~dp0 -o %~dp0..\..\build\loader.bin -l %~dp0..\..\build\loader.lst
+if not exist %~dp0..\..\build\loader.bin (
+echo compile error!
+goto end
+)
+) else (
+echo ERROR: ..\..\tools\nasm\nasm.exe not found!
+)
+goto end
+
+:end
+echo leaving directory %~dp0
