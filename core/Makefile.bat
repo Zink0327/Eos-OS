beca3bbda800bc7d11f8edce389dcaba05007ef5
Zink0327 <122519365+Zink0327@users.noreply.github.com>
2024-04-14 15:27:26 +0800

    Add files via upload

--- core/Makefile.bat
+++ core/Makefile.bat
@@ -23,13 +23,15 @@ if "%1" equ "clean" (goto clean) else (goto build)
 
 :build
 
+copy %~dp0init\%ARCH%\%BOOTWAY%\* %~dp0
+
 if exist %WORKROOT%tools\gcc\bin\%CC% (
 echo %WORKROOT%tools\gcc\bin\%CC% -mcmodel=large -fno-builtin -m64 -c %~dp0print.c -o %WORKROOT%build\print.o -fno-stack-protector
 %WORKROOT%tools\gcc\bin\%CC% -mcmodel=large -fno-builtin -m64 -c %~dp0print.c -o %WORKROOT%build\print.o -fno-stack-protector
 
 if not exist %WORKROOT%build\print.o (
 set ERRFLAG="1"
-echo compile error in print.c!
+echo error in compiling print.c!
 goto end
 )
 echo %WORKROOT%tools\gcc\bin\%CC% -mcmodel=large -fno-builtin -m64 -c %~dp0main.c -o %WORKROOT%build\main.o -fno-stack-protector
@@ -37,10 +39,27 @@ echo %WORKROOT%tools\gcc\bin\%CC% -mcmodel=large -fno-builtin -m64 -c %~dp0main.
 
 if not exist %WORKROOT%build\main.o (
 set ERRFLAG="1"
-echo compile errorin main.c!
+echo error in compiling main.c!
 goto end
 )
 
+echo %WORKROOT%tools\gcc\bin\%CC% -mcmodel=large -fno-builtin -m64 -c %~dp0init.c -o %WORKROOT%build\init.o -fno-stack-protector
+%WORKROOT%tools\gcc\bin\%CC% -mcmodel=large -fno-builtin -m64 -c %~dp0init.c -o %WORKROOT%build\init.o -fno-stack-protector
+
+if not exist %WORKROOT%build\init.o (
+set ERRFLAG="1"
+echo error in compiling init.c!
+goto end
+)
+
+echo %WORKROOT%tools\gcc\bin\%CC% -E  %~dp0head.S > %WORKROOT%build\head.s
+%WORKROOT%tools\gcc\bin\%CC% -E  %~dp0head.S > %WORKROOT%build\head.s
+
+if not exist %WORKROOT%build\head.s (
+set ERRFLAG="1"
+echo error in generating head.S!
+goto end
+)
 
 
 ) else (
@@ -49,14 +68,21 @@ echo FATAL ERROR: %WORKROOT%tools\gcc\bin\%CC% not found!
 goto end
 )
 
-call %~dp0\init\%ARCH%\%BOOTWAY%\Makefile.bat
+if exist %WORKROOT%tools\gcc\bin\%AS% (
+echo %WORKROOT%tools\gcc\bin\%AS% --64 %WORKROOT%build\head.s -o %WORKROOT%build\head.o
+%WORKROOT%tools\gcc\bin\%AS% --64 %WORKROOT%build\head.s -o %WORKROOT%build\head.o
 
-if %ERRFLAG% equ "1" (
-echo ***********An error occured***********
-echo       please check the info above
-echo **************************************
+if not exist %WORKROOT%build\head.o (
+set ERRFLAG="1"
+echo error in compiling head.s!
 goto end
 )
+) else (
+set ERRFLAG="1"
+echo FATAL ERROR: %WORKROOT%tools\gcc\bin\%AS% not found!
+goto end
+)
+
 
 if exist %WORKROOT%tools\gcc\bin\%LD% (
 echo %WORKROOT%tools\gcc\bin\%LD% -b %LDLINKFMT% -z muldefs -o %WORKROOT%build\kernel.elf %WORKROOT%build\head.o %WORKROOT%build\main.o %WORKROOT%build\print.o %WORKROOT%build\init.o  -T %~dp0linkkernel.lds 
@@ -64,7 +90,7 @@ echo %WORKROOT%tools\gcc\bin\%LD% -b %LDLINKFMT% -z muldefs -o %WORKROOT%build\k
 
 if not exist %WORKROOT%build\kernel.elf (
 set ERRFLAG="1"
-echo compile error in linking!
+echo error in linking!
 goto end
 )
 ) else (
