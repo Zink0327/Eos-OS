7aee899db74099fa5976c1360f3fa5e1f3086bd5
Zink0327 <122519365+Zink0327@users.noreply.github.com>
2024-06-27 22:28:46 +0800

    Create defines.h

---
+++ core/init/x86_64/BIOS/defines.h
@@ -0,0 +1,29 @@
+/*----------EOS kernel initial function for BIOS----------
+    File name:BIOS/init.h
+
+    Copyright (C) 2023 by Zink
+    This file is part of EOS
+
+    EOS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    EOS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _EOS_X86_64_TYPEDEF
+#define _EOS_X86_64_TYPEDEF
+
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef unsigned long uint32_t;
+
+#endif
